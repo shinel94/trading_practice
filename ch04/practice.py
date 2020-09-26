1d38c91e1645d0364dbfe1ef080029252e218989
shinel94 <or_abel@naver.com>
2020-09-27 03:37:52 +0900

    누락된 문제 4 - 9 추가

--- ch04/practice.py
+++ ch04/practice.py
@@ -43,6 +43,17 @@ def pb4_8():
         result = ' '*(5 - i - 1) + '*'*(2*i+1) + ' '*(5 - i - 1)
         print(result)
 
+
+def pb4_9():
+    apart = [[101, 102, 103, 104], [201, 202, 203, 204], [301, 302, 303, 304], [401, 402, 403, 404]]
+    arrears = [101, 203, 301, 404]
+    for apart_floor in apart:
+        for room in apart_floor:
+            if room in arrears:
+                print(f'{room}은 구독료가 미납되어 신문이 배달되지 않습니다.')
+            else:
+                print(f'{room} 신문 배달 완료')
+
 if __name__ == '__main__':
     pb4_1()
     pb4_2()
@@ -52,3 +63,4 @@ if __name__ == '__main__':
     pb4_6()
     pb4_7()
     pb4_8()
+    pb4_9()
